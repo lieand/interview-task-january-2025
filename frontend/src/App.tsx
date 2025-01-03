ce484a078183b50149d50c666e2203c068f20a89
lieand <a2akl2@hotmail.com>
2025-01-03 13:30:05 +0100

    Reformating component size ratio

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,12 @@ import DeviceList from "./components/DeviceList";
 function App(): React.JSX.Element {
   return (
     <div className="w-screen h-screen flex">
-      <DeviceList />
-      <Map />
+      <div className="w-1/3 h-full overflow-auto border-r">
+        <DeviceList />
+      </div>
+      <div className="w-2/3 h-full">
+        <Map />
+      </div>
     </div>
   );
 }
