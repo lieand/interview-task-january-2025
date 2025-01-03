ba38e83e81994bb2e598c79c885bf961f1f3026e
lieand <a2akl2@hotmail.com>
2025-01-03 13:26:54 +0100

    Added API endpoint for device fetching

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import path from "path";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
@@ -13,6 +15,18 @@ app.get("/", (req: Request, res: Response) => {
 app.get("/test", (req: Request, res: Response) => {
   res.send([{ name: "test" }, { name: "test123" }, { name: "321" }]);
 });
+
+app.get("/data/devices", (req: Request, res: Response) => {
+    const devicesPath = path.join(__dirname, "data/devices.json");
+    fs.readFile(devicesPath, "utf8", (err, data) => {
+        if(err) {
+            res.status(500).send({error: "Could not read devices file"});
+        } else {
+            res.json(JSON.parse(data));
+        }
+        });
+});
+
 
 app.listen(port, () => {
   console.log(`[backend]: Server is running at port: ${port}`);
